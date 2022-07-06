b91283bb29eb9726c91f5fb1f572b755c9f039c9
IANONA Innocent <ianona.innocent>
2022-07-06 14:08:09 +0100

    Healthcheck endpoint

--- controllers/ExampleController.ts
+++ controllers/ExampleController.ts
@@ -1,4 +1,6 @@
 import {FastifyInstance} from "fastify";
+import pkg from "../package.json";
+
 
 export default class ExampleController {
 
@@ -7,8 +9,20 @@ export default class ExampleController {
     constructor(router: FastifyInstance) {
         this.router = router
 
-        router.get('/api',
-            this.sayHello.bind(this))
+        router.get('/api',this.sayHello.bind(this))
+
+        router.get('/api/healthcheck',(req, res)=>{
+            const healthcheck = {
+                "name": pkg.name,
+                "version": pkg.version,
+                "time": Date.now()
+            };
+            try {
+                res.send(healthcheck);
+            } catch (e) {
+                res.status(503).send();
+            }
+        });
     }
 
     async sayHello(): Promise<string> {
